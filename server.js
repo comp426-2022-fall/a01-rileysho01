fa22d7dab2224b249d39f5057ad5439e60c76382
rileysho01 <69476901+rileysho01@users.noreply.github.com>
2022-08-31 19:09:43 -0400

    Testing without minimist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-const mini = require("minimist");
+//const mini = require("minimist");
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 //node app.js --port=;
